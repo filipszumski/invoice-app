1f55448edd6babc1792f80cac3a138eae6da2a5e
Filip Szumski <filipszumski@gmail.com>
2021-05-27 23:12:16 +0200

    changed last input to span to display total item amount and created useEffect to update item total state after chnge of item price or quantity input value

--- src/components/Form/ItemList/index.js
+++ src/components/Form/ItemList/index.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../../Button";
 import { Input } from "./Input";
 
 export const ItemList = ({ invoice, setInvoice }) => {
+    const [effectLauncher, setEffectLauncher] = useState({
+        number: Math.random(),
+        index: "",
+    });
     const onAddItemListButtonChange = () => setInvoice({
         ...invoice,
         items: [
             ...invoice.items,
             {
                 name: "",
-                quantity: 0,
+                quantity: "",
                 price: "",
                 total: ""
             }
         ]
     });
+
+    useEffect(() => {
+        // MAYBE MAKE TOTAL AMOUNT OF ITEMS ON SUMBIT - ITS NOT NECESARRY TO DO I ON EVERY INPUT CHANGE
+        // TOTAL IN ITEM IS NECESSARY BECAUSE IT WILL DISPLAY VAULUE TO USER IN FORM 
+        console.log("ITEM TOTAL EFFECT HAS BEEN ");
+
+        setInvoice(invoice => ({
+            ...invoice,
+            items: invoice.items.map((item, index) => {
+                if (effectLauncher.index === index) {
+                    return {
+                        ...item,
+                        total: item.quantity * item.price,
+                    }
+                }
+                return {
+                    ...item,
+                }
+            }),
+        }))
+    }, [effectLauncher, setInvoice]);
 
     return (
         <section>
@@ -37,6 +62,7 @@ export const ItemList = ({ invoice, setInvoice }) => {
                                 liItem={item}
                                 invoice={invoice}
                                 setInvoice={setInvoice}
+                                setEffectLauncher={setEffectLauncher}
                             />
                             <Input
                                 name="quantity"
@@ -45,6 +71,7 @@ export const ItemList = ({ invoice, setInvoice }) => {
                                 liItem={item}
                                 invoice={invoice}
                                 setInvoice={setInvoice}
+                                setEffectLauncher={setEffectLauncher}
                             />
                             <Input
                                 name="price"
@@ -53,15 +80,9 @@ export const ItemList = ({ invoice, setInvoice }) => {
                                 liItem={item}
                                 invoice={invoice}
                                 setInvoice={setInvoice}
+                                setEffectLauncher={setEffectLauncher}
                             />
-                            <Input
-                                name="total"
-                                type="number"
-                                liIndex={index}
-                                liItem={item}
-                                invoice={invoice}
-                                setInvoice={setInvoice}
-                            />
+                            <span>{invoice.items[index].total || ""}</span>
                         </li>
                     )
                 })}
