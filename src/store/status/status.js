7c29b14d437cefece9cd88ce0e1fb645c73715f7
Filip Szumski <filipszumski@gmail.com>
2021-05-30 17:12:56 +0200

    created 2 action that are changing status tosuccess or error during fetching data

--- src/store/status/status.js
+++ src/store/status/status.js
@@ -1,13 +1,40 @@
+const GET_INVOICE_STATUS_SUCCESS = "GET_INVOICE_STATUS_SUCCESS";
+const GET_INVOICE_STATUS_ERROR = "GET_INVOICE_STATUS_ERROR";
 
 const initialStatus = {
-    loading: false,
-    error: false,
+    status: "loading",
     overlayActive: false,
     formActive: false,
     deleteInvoiceActive: false,
 };
 
-export const status = (state = initialStatus, action) => {
+export const getInvoicesStatusSuccess = () => {
+    return {
+        type: GET_INVOICE_STATUS_SUCCESS,
+    }
+}
 
-    return state;
+export const getInvoicesStatusError = () => {
+    return {
+        type: GET_INVOICE_STATUS_ERROR,
+    }
+}
+
+
+export const status = (state = initialStatus, action) => {
+    switch (action.type) {
+        case GET_INVOICE_STATUS_SUCCESS:
+            return {
+                ...state,
+                status: "success"
+            };
+        case GET_INVOICE_STATUS_ERROR:
+            return {
+                ...state,
+                status: "error" 
+            };
+        default:
+            return state;
+    }
+
 }
