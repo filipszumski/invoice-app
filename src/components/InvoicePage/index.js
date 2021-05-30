b2817f17b040f5091cef95de9651ae6e6cfbfb4e
Filip Szumski <filipszumski@gmail.com>
2021-05-30 23:21:39 +0200

    changed data on page for invoice data user clicked, filtred with id using useParams

--- src/components/InvoicePage/index.js
+++ src/components/InvoicePage/index.js
@@ -1,74 +1,115 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router";
+import { format } from "date-fns";
 import { Button } from "../Button";
 
 export const InvoicePage = () => {
+    const history = useHistory();
+    const params = useParams();
+    const status = useSelector(state => state.status);
+    const invoice = useSelector(state => state.invoices.filter(invoice => invoice.id === params.id))[0];
+    console.log(status);
+    console.log(invoice);
+
+    const formatDate = (dateString) => {
+        const dateArrayStrings = dateString.split("-");
+        const dateArrayNumbers = dateArrayStrings.map(item => parseInt(item, 10));
+        return format(new Date(dateArrayNumbers[0], dateArrayNumbers[1], dateArrayNumbers[2]), "d MMM y");
+
+    };
+
+    const onGoBackButtonClick = () => {
+        return history.goBack();
+    };
+
+    formatDate("2021-08-18");
 
     return (
         <main>
             {/* add nice arrow */}
-            <Button content="Go back" extraContent="<" />
+            <Button onClick={onGoBackButtonClick} content="Go back" extraContent="<" />
             <section>
-                <p>
-                    <span>Status</span>
-                    <span>Pending</span>
-                </p>
-                <p>
-                    <Button content="Edit" />
-                    <Button content="Delete" />
-                    <Button content="Mark as Paid" />
-                </p>
-            </section>
-            <section>
-                <div>
-                    <header>
-                        <h2>#XM9141</h2>
-                        <span>Graphic Designer</span>
-                    </header>
-                    <p>
-                        <span>19 Union Terrace</span>
-                        <span>London</span>
-                        <span>E1 3EZ</span>
-                        <span>United Kingdom</span>
-                    </p>
-                </div>
-                <div>
-                    <div>
-                        <p>
-                            <span>Invoice Date</span>
-                            <span>21 AUG 2021</span>
-                        </p>
-                        <p>
-                            <span>Invoice Date</span>
-                            <span>21 AUG 2021</span>
-                        </p>
-                    </div>
-                    <p>
-                        <span>Bill To</span>
-                        <span>Alex Grim</span>
-                        <span>19 Union Terrace</span>
-                        <span>London</span>
-                        <span>E1 3EZ</span>
-                        <span>United Kingdom</span>
-                    </p>
-                    <p>
-                        <span>Sent To</span>
-                        <span>alexgrim@mail.com</span>
-                    </p>
-                </div>
-                <div>
-                    <ul>
-                        <li>
-                            <span>Item Name</span>
-                            <span>Qty.</span>
-                            <span>Price</span>
-                            <span>Total</span>
-                        </li>
-                    </ul>
-                    <p>
-                        <span>Amount Due</span>
-                        <span>$ 566.00</span>
-                    </p>
-                </div>
+                {status.status === "loading"
+                    ? <p>Loading in progress...</p>
+                    : status.status === "error"
+                        ? <p>Error occurred</p>
+                        : (
+                            <>
+                                <section>
+                                    <p>
+                                        <span>Status</span>
+                                        <span>{invoice.status}</span>
+                                    </p>
+                                    <p>
+                                        <Button content="Edit" />
+                                        <Button content="Delete" />
+                                        <Button content="Mark as Paid" />
+                                    </p>
+                                </section>
+                                <section>
+                                    <div>
+                                        <header>
+                                            <h2>{invoice.id}</h2>
+                                            <span>{invoice.description}</span>
+                                        </header>
+                                        <p>
+                                            <span>{invoice.senderAddress.street}</span>
+                                            <span>{invoice.senderAddress.city}</span>
+                                            <span>{invoice.senderAddress.postCode}</span>
+                                            <span>{invoice.senderAddress.country}</span>
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <p>
+                                                <span>Invoice Date</span>
+                                                <span>{formatDate(invoice.createdAt)}</span>
+                                            </p>
+                                            <p>
+                                                <span>Payment Due</span>
+                                                <span>{formatDate(invoice.paymentDue)}</span>
+                                            </p>
+                                        </div>
+                                        <p>
+                                            <span>Bill To</span>
+                                            <span>{invoice.clientName}</span>
+                                            <span>{invoice.clientAddress.street}</span>
+                                            <span>{invoice.clientAddress.city}</span>
+                                            <span>{invoice.clientAddress.postCode}</span>
+                                            <span>{invoice.clientAddress.country}</span>
+                                        </p>
+                                        <p>
+                                            <span>Sent To</span>
+                                            <span>{invoice.clientEmail}</span>
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                                <span>Item Name</span>
+                                                <span>Qty.</span>
+                                                <span>Price</span>
+                                                <span>Total</span>
+                                            </li>
+                                            {invoice.items.map((item, index) => (
+                                                <li key={index}>
+                                                    <span>{item.name}</span>
+                                                    <span>{item.quantity}</span>
+                                                    <span>{item.price}</span>
+                                                    <span>{item.total}</span>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                        <p>
+                                            <span>Amount Due</span>
+                                            <span>${invoice.total}</span>
+                                        </p>
+                                    </div>
+                                </section>
+                            </>
+                        )}
+
             </section>
         </main>
     )
