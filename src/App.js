9786e5af2fc74fb41068094049d305d116738380
Filip Szumski <filipszumski@gmail.com>
2021-05-30 17:14:31 +0200

    used useEffect hook to get invoice data from API and dispatch actions depending what the response is

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 import { InvoicesPage } from "./components/InvoicesPage";
 import { Form } from "./components/Form";
 import { InvoicePage } from "./components/InvoicePage";
 import { DeleteInvoiceWindow } from "./components/DeleteInvoiceWindow";
 import { Overlay } from "./components/Overlay/styled";
+import { getInvoices } from "./services/invoices";
+import { getInvoicesDataSuccess } from "./store/invoices/invoices";
+import { getInvoicesStatusError, getInvoicesStatusSuccess } from "./store/status/status";
 
 function App() {
+  const dispatch = useDispatch();
+  const timeoutId = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getInvoices();
+        dispatch(getInvoicesDataSuccess(response))
+        dispatch(getInvoicesStatusSuccess());
+      }
+      catch (error) {
+        console.error(error);
+        dispatch(getInvoicesStatusError())
+      }
+    };
+
+    timeoutId.current = setTimeout(fetchData, 1000);
+
+    return () => clearTimeout(timeoutId.current);
+  }, [])
+
   return (
     <>
       <Overlay active={false} />
