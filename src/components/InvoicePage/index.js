b874c7ee15cc1f67fe3a20c4dcecd6a5497ff75d
Filip Szumski <filipszumski@gmail.com>
2021-06-03 19:17:42 +0200

    used Form and DeleteInvoiceWindow

--- src/components/InvoicePage/index.js
+++ src/components/InvoicePage/index.js
@@ -1,116 +1,121 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router";
 import { format } from "date-fns";
 import { Button } from "../Button";
+import { Form } from "../Form/";
+import { DeleteInvoiceWindow } from "../DeleteInvoiceWindow";
+import { displayForm } from "../../store/status/status";
 
 export const InvoicePage = () => {
     const history = useHistory();
+    const dispatch = useDispatch();
     const params = useParams();
     const status = useSelector(state => state.status);
     const invoice = useSelector(state => state.invoices.filter(invoice => invoice.id === params.id))[0];
-    console.log(status);
-    console.log(invoice);
 
+    // MOMENT.JS, LUXON
     const formatDate = (dateString) => {
         const dateArrayStrings = dateString.split("-");
         const dateArrayNumbers = dateArrayStrings.map(item => parseInt(item, 10));
         return format(new Date(dateArrayNumbers[0], dateArrayNumbers[1], dateArrayNumbers[2]), "d MMM y");
-
     };
+
+    const onEditButtonClick = () => dispatch(displayForm(true))
 
     const onGoBackButtonClick = () => {
         return history.goBack();
     };
-
-    formatDate("2021-08-18");
-
     return (
-        <main>
-            {/* add nice arrow */}
-            <Button onClick={onGoBackButtonClick} content="Go back" extraContent="<" />
-            <section>
-                {status.status === "loading"
-                    ? <p>Loading in progress...</p>
-                    : status.status === "error"
-                        ? <p>Error occurred</p>
-                        : (
-                            <>
-                                <section>
-                                    <p>
-                                        <span>Status</span>
-                                        <span>{invoice.status}</span>
-                                    </p>
-                                    <p>
-                                        <Button content="Edit" />
-                                        <Button content="Delete" />
-                                        <Button content="Mark as Paid" />
-                                    </p>
-                                </section>
-                                <section>
-                                    <div>
-                                        <header>
-                                            <h2>{invoice.id}</h2>
-                                            <span>{invoice.description}</span>
-                                        </header>
+        <>
+            <main>
+                {/* add nice arrow */}
+                <Button onClick={onGoBackButtonClick} content="Go back" extraContent="<" />
+                <section>
+                    {status.status === "loading"
+                        ? <p>Loading in progress...</p>
+                        : status.status === "error"
+                            ? <p>Error occurred</p>
+                            : (
+                                <>
+                                    <section>
                                         <p>
-                                            <span>{invoice.senderAddress.street}</span>
-                                            <span>{invoice.senderAddress.city}</span>
-                                            <span>{invoice.senderAddress.postCode}</span>
-                                            <span>{invoice.senderAddress.country}</span>
+                                            <span>Status</span>
+                                            <span>{invoice.status}</span>
                                         </p>
-                                    </div>
-                                    <div>
+                                        <p>
+                                            <Button onClick={onEditButtonClick} content="Edit" />
+                                            <Button content="Delete" />
+                                            <Button content="Mark as Paid" />
+                                        </p>
+                                    </section>
+                                    <section>
                                         <div>
+                                            <header>
+                                                <h2>{invoice.id}</h2>
+                                                <span>{invoice.description}</span>
+                                            </header>
                                             <p>
-                                                <span>Invoice Date</span>
-                                                <span>{formatDate(invoice.createdAt)}</span>
-                                            </p>
-                                            <p>
-                                                <span>Payment Due</span>
-                                                <span>{formatDate(invoice.paymentDue)}</span>
+                                                <span>{invoice.senderAddress.street}</span>
+                                                <span>{invoice.senderAddress.city}</span>
+                                                <span>{invoice.senderAddress.postCode}</span>
+                                                <span>{invoice.senderAddress.country}</span>
                                             </p>
                                         </div>
-                                        <p>
-                                            <span>Bill To</span>
-                                            <span>{invoice.clientName}</span>
-                                            <span>{invoice.clientAddress.street}</span>
-                                            <span>{invoice.clientAddress.city}</span>
-                                            <span>{invoice.clientAddress.postCode}</span>
-                                            <span>{invoice.clientAddress.country}</span>
-                                        </p>
-                                        <p>
-                                            <span>Sent To</span>
-                                            <span>{invoice.clientEmail}</span>
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <ul>
-                                            <li>
-                                                <span>Item Name</span>
-                                                <span>Qty.</span>
-                                                <span>Price</span>
-                                                <span>Total</span>
-                                            </li>
-                                            {invoice.items.map((item, index) => (
-                                                <li key={index}>
-                                                    <span>{item.name}</span>
-                                                    <span>{item.quantity}</span>
-                                                    <span>{item.price}</span>
-                                                    <span>{item.total}</span>
+                                        <div>
+                                            <div>
+                                                <p>
+                                                    <span>Invoice Date</span>
+                                                    <span>{formatDate(invoice.createdAt)}</span>
+                                                </p>
+                                                <p>
+                                                    <span>Payment Due</span>
+                                                    <span>{formatDate(invoice.paymentDue)}</span>
+                                                </p>
+                                            </div>
+                                            <p>
+                                                <span>Bill To</span>
+                                                <span>{invoice.clientName}</span>
+                                                <span>{invoice.clientAddress.street}</span>
+                                                <span>{invoice.clientAddress.city}</span>
+                                                <span>{invoice.clientAddress.postCode}</span>
+                                                <span>{invoice.clientAddress.country}</span>
+                                            </p>
+                                            <p>
+                                                <span>Sent To</span>
+                                                <span>{invoice.clientEmail}</span>
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <ul>
+                                                <li>
+                                                    <span>Item Name</span>
+                                                    <span>Qty.</span>
+                                                    <span>Price</span>
+                                                    <span>Total</span>
                                                 </li>
-                                            ))}
-                                        </ul>
-                                        <p>
-                                            <span>Amount Due</span>
-                                            <span>${invoice.total}</span>
-                                        </p>
-                                    </div>
-                                </section>
-                            </>
-                        )}
+                                                {invoice.items.map((item, index) => (
+                                                    <li key={index}>
+                                                        <span>{item.name}</span>
+                                                        <span>{item.quantity}</span>
+                                                        <span>{item.price}</span>
+                                                        <span>{item.total}</span>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                            <p>
+                                                <span>Amount Due</span>
+                                                <span>${invoice.total}</span>
+                                            </p>
+                                        </div>
+                                    </section>
+                                </>
+                            )}
 
-            </section>
-        </main>
+                </section>
+            </main>
+            <Form id={params.id} />
+            <DeleteInvoiceWindow />
+        </>
     )
 }
