2982d44a3fa15695dcf0cf649a1987ffe7a5fec2
Filip Szumski <filipszumski@gmail.com>
2021-06-04 15:58:08 +0200

    moved onDeleteInvoiceButtonClick to InvoicePage component

--- src/components/DeleteInvoiceWindow/index.js
+++ src/components/DeleteInvoiceWindow/index.js
@@ -1,33 +1,15 @@
 import React from "react";
 import { Button } from "../Button";
 import { Wrapper } from "./styled";
-import { displayDeleteInvoiceAlert, setStatus } from "../../store/status/status";
-import { deleteInvoice } from "../../services/invoices";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router";
-import { toInvoices } from "../../routes";
 
-export const DeleteInvoiceWindow = ({ id, active }) => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-
-    const onDeleteInvoiceButtonClick = async () => {
-        try {
-            await deleteInvoice(id);
-            dispatch(setStatus("loading"));
-            dispatch(displayDeleteInvoiceAlert(false));
-            history.push(toInvoices());
-        } catch (error) {
-            console.error(error);
-        }
-    };
+export const DeleteInvoiceWindow = ({ onDeleteInvoiceButtonClick, onDeleteButtonClick, active }) => {
 
     return (
         <Wrapper active={active}>
             <h2>Confirm Deletion</h2>
             <p>Are you sure you want to delete invoice #XM9141? This action cannot be undone.</p>
             <p>
-                <Button onClick={() => dispatch(displayDeleteInvoiceAlert(false))} content={"Cancel"} />
+                <Button onClick={() => onDeleteButtonClick(false)} content={"Cancel"} />
                 <Button onClick={onDeleteInvoiceButtonClick} content={"Delete"} />
             </p>
         </Wrapper>
