88aa10cfef590f2352223bd712404b08c8d3d12b
Filip Szumski <filipszumski@gmail.com>
2021-06-06 22:04:37 +0200

    modified message displaying invoices quantity

--- src/components/InvoicesPage/index.js
+++ src/components/InvoicesPage/index.js
@@ -22,8 +22,7 @@ export const InvoicesPage = () => {
                             <header>
                                 <div>
                                     <h1>Invoices</h1>
-                                    <p>There are {invoices.length} total invoices</p>
-                                    {/* or no incove text */}
+                                    <p>{invoices.length ? `There are ${invoices.length} total invoices` : "No invoices"}</p>
                                 </div>
                                 <div>
                                     <div>
