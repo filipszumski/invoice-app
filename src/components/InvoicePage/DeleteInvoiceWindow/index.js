02c330c4d431597bf02557362311390089871c97
Filip Szumski <filipszumski@gmail.com>
2021-06-06 22:03:13 +0200

    updated Button import

--- src/components/InvoicePage/DeleteInvoiceWindow/index.js
+++ src/components/InvoicePage/DeleteInvoiceWindow/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "../Button";
+import { Button } from "../../Button";
 import { Wrapper } from "./styled";
 
 export const DeleteInvoiceWindow = ({ onDeleteInvoiceButtonClick, onDeleteButtonClick, active }) => {
