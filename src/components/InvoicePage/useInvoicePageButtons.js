839e98e5c40c6903f81714b5b95ac8f2b03d18ef
Filip Szumski <filipszumski@gmail.com>
2021-06-07 13:40:51 +0200

    removed getDataActive action, added update invoice action in function

--- src/components/InvoicePage/useInvoicePageButtons.js
+++ src/components/InvoicePage/useInvoicePageButtons.js
@@ -2,7 +2,8 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { toInvoices } from "../../routes";
 import { deleteInvoice, patchInvoice } from "../../services/invoices";
-import { displayForm, displayDeleteInvoiceAlert, setStatus, getDataActive } from "../../store/status/status";
+import { updateInvoiceDataSuccess } from "../../store/invoice/invoice";
+import { displayForm, displayDeleteInvoiceAlert, setStatus } from "../../store/status/status";
 
 export const useInvoicePageButtons = (params, invoice) => {
     const history = useHistory();
@@ -10,20 +11,16 @@ export const useInvoicePageButtons = (params, invoice) => {
 
     const onEditButtonClick = () => dispatch(displayForm(true));
 
-    const onGoBackButtonClick = () => {
-        dispatch(setStatus("loading"));
-        dispatch(getDataActive(true));
-    };
+    const onGoBackButtonClick = () => dispatch(setStatus("loading"));
 
     const onDeleteButtonClick = (active) => dispatch(displayDeleteInvoiceAlert(active));
 
     const onDeleteInvoiceButtonClick = async () => {
         try {
             dispatch(setStatus("loading"));
-            await deleteInvoice(params.id);
             dispatch(displayDeleteInvoiceAlert(false));
+            await deleteInvoice(params.id);
             history.push(toInvoices());
-            dispatch(getDataActive(true));
         } catch (error) {
             console.error(error);
             dispatch(setStatus("error"));
@@ -33,11 +30,12 @@ export const useInvoicePageButtons = (params, invoice) => {
     const markAsPaid = async () => {
         try {
             dispatch(setStatus("loading"));
-            await patchInvoice(params.id, {
+            const response = await patchInvoice(params.id, {
                 ...invoice,
                 status: "paid",
             });
-            dispatch(getDataActive(true));
+            dispatch(updateInvoiceDataSuccess(response));
+            dispatch(setStatus("success"));
         } catch (error) {
             console.error(error);
             dispatch(setStatus("error"));
