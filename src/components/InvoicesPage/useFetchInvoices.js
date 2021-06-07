a9744dbba3b110c44f01aace8f5a1d932d21a3b9
Filip Szumski <filipszumski@gmail.com>
2021-06-07 13:41:37 +0200

    removed getDataActive action and cleared dependency array

--- src/components/InvoicesPage/useFetchInvoices.js
+++ src/components/InvoicesPage/useFetchInvoices.js
@@ -1,20 +1,17 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getInvoices } from "../../services/invoices";
 import { getInvoicesDataSuccess } from "../../store/invoices/invoices";
-import { getDataActive, setStatus } from "../../store/status/status";
+import { setStatus } from "../../store/status/status";
 
 export const useFetchInvoices = () => {
-    const status = useSelector(state => state.status);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        console.log("Fetch Invoices Effect");
         const fetchData = async () => {
             try {
                 const response = await getInvoices();
                 dispatch(getInvoicesDataSuccess(response));
-                dispatch(getDataActive(false));
                 dispatch(setStatus("success"));
             }
             catch (error) {
@@ -22,13 +19,6 @@ export const useFetchInvoices = () => {
                 dispatch(setStatus("error"));
             }
         };
-
-        if (!status.getDataActive) {
-            console.log("Fectch Invoices Effect RETURN");
-            return;
-        };
-
         fetchData();
-
-    }, [status.getDataActive, dispatch])
+    }, [dispatch])
 };
