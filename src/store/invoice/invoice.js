1d50f120c390af7528a4fb349cdf2e2c51fe0acd
Filip Szumski <filipszumski@gmail.com>
2021-06-07 13:42:31 +0200

    added updateInvoiceDataSuccess action

--- src/store/invoice/invoice.js
+++ src/store/invoice/invoice.js
@@ -1,8 +1,16 @@
 const GET_INVOICE_DATA_SUCCESS = "GET_INVOICE_DATA_SUCCESS";
+const UPDATE_INVOICE_DATA_SUCCESS = "UPDATE_INVOICE_DATA_SUCCESS"
 
 export const getInvoiceDataSuccess = (invoice) => {
     return {
         type: GET_INVOICE_DATA_SUCCESS,
+        payload: invoice,
+    }
+};
+
+export const updateInvoiceDataSuccess = (invoice) => {
+    return {
+        type: UPDATE_INVOICE_DATA_SUCCESS,
         payload: invoice,
     }
 };
@@ -12,6 +20,8 @@ const initialInvoices = {};
 export const invoice = (state = initialInvoices, action) => {
     switch (action.type) {
         case GET_INVOICE_DATA_SUCCESS:
+            return action.payload;
+        case UPDATE_INVOICE_DATA_SUCCESS:
             return action.payload;
         default:
             return state;
