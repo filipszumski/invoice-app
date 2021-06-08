47f578fc009b8c3566fbef942f914b15d9f4de5a
Filip Szumski <filipszumski@gmail.com>
2021-06-08 15:42:34 +0200

    added event listener to close filter popup on outside popup window click

--- src/components/InvoicesPage/Filter/index.js
+++ src/components/InvoicesPage/Filter/index.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import { logDOM } from "@testing-library/dom";
+import React, { useEffect, useRef, useState } from "react";
 import { Wrapper, Options, Button } from "./styled";
 
 export const Filter = ({ filters, setFilters }) => {
     const [active, setActive] = useState(false);
+    const popupRef = useRef(null);
+
+    useEffect(() => {
+        const onClick = ({ target }) => {
+            if (popupRef.current === target) {
+                return;
+            }
+            if (popupRef.current.contains(target)) {
+                return;
+            }
+            return setActive(false);
+        };
+
+        if (active) {
+            window.addEventListener("click", onClick)
+        }
+        return () => window.removeEventListener("click", onClick);
+    }, [active])
+
     const onInputChange = ({ target }) => setFilters(filters.map((item) => {
         if (target.name !== item.name) {
-            return {...item};
+            return { ...item };
         }
         return {
             ...item,
@@ -16,7 +36,7 @@ export const Filter = ({ filters, setFilters }) => {
     return (
         <Wrapper>
             <Button onClick={() => setActive(!active)}>Filter by status</Button>
-            <Options active={active}>
+            <Options ref={popupRef} active={active}>
                 {filters.map(filter => (
                     <p key={filter.id}>
                         <input
