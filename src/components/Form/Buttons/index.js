e66963e1962b5406c021e4ec2f508b5c97802a1b
Filip Szumski <filipszumski@gmail.com>
2021-06-30 23:24:09 +0200

    changed functions to close and discard changes names

--- src/components/Form/Buttons/index.js
+++ src/components/Form/Buttons/index.js
@@ -1,17 +1,22 @@
 import { Button } from "../../Button";
 
-export const Buttons = ({ id, onSubmitInvoiceButtonClick, onSubmitInvoiceUpdateButtonClick, onCloseFormButtonClick }) => {
-
+export const Buttons = ({
+    id,
+    onSubmitInvoiceButtonClick,
+    onSubmitInvoiceUpdateButtonClick,
+    onCancelButtonClick,
+    onDiscardChangesButtonClick
+}) => {
     return (
         <section>
             {id ?
                 <>
-                    <Button type="button" onClick={onCloseFormButtonClick} content={"Cancel"} />
+                    <Button type="button" onClick={onCancelButtonClick} content={"Cancel"} />
                     <Button type="button" onClick={onSubmitInvoiceUpdateButtonClick} content={"Save Changes"} />
                 </>
                 :
                 <>
-                    < Button onClick={onCloseFormButtonClick} type="button" content={"Discard"} />
+                    < Button onClick={onDiscardChangesButtonClick} type="button" content={"Discard"} />
                     <div>
                         <Button type="button" onClick={() => onSubmitInvoiceButtonClick("draft")} content={"Save as Draft"} />
                         <Button type="button" onClick={() => onSubmitInvoiceButtonClick("pending")} content={"Save & Send"} />
