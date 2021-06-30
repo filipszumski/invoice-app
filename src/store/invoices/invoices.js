928b3de8bae6a4e483f96a354900bd6a82cd20fb
Filip Szumski <filipszumski@gmail.com>
2021-06-30 23:27:07 +0200

    deleted add invoice and remove invoice actions

--- src/store/invoices/invoices.js
+++ src/store/invoices/invoices.js
@@ -1,25 +1,9 @@
 const GET_INVOICES_DATA_SUCCESS = "GET_INVOICES_DATA_SUCCESS";
-const ADD_INVOICE_DATA_SUCCESS = "ADD_INVOICE_DATA_SUCCESS";
-const REMOVE_INVOICE_DATA_SUCCESS = "REMOVE_INVOICE_DATA_SUCCESS";
 
 export const getInvoicesDataSuccess = (invoices) => {
     return {
         type: GET_INVOICES_DATA_SUCCESS,
         payload: invoices
-    }
-};
-
-export const addInvoiceDataSuccess = (invoices) => {
-    return {
-        type: ADD_INVOICE_DATA_SUCCESS,
-        payload: invoices
-    }
-};
-
-export const removeInvoiceDataSuccess = (id) => {
-    return {
-        type: REMOVE_INVOICE_DATA_SUCCESS,
-        payload: id
     }
 };
 
@@ -29,13 +13,7 @@ export const invoices = (state = initialInvoices, action) => {
     switch (action.type) {
         case GET_INVOICES_DATA_SUCCESS:
             return action.payload;
-        case ADD_INVOICE_DATA_SUCCESS:
-            return [
-                ...state,
-                action.payload,
-            ];
-        case REMOVE_INVOICE_DATA_SUCCESS:
-            return state.filter(invoice => invoice.id !== action.payload);
+
         default:
             return state;
     }
