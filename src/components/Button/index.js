cee5da9be5024fb62f3607567a7e9d13201e6bee
Filip Szumski <filipszumski@gmail.com>
2021-07-05 22:34:48 +0200

    added data-* atribute to button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export const Button = ({ content, extraContent, onClick, type }) => {
+export const Button = ({ content, extraContent, onClick, type, status }) => {
 
     return (
         <button
             onClick={onClick}
             type={type}
+            data-status={status}
         >
             {extraContent ? extraContent : ""} {content}
         </button>
