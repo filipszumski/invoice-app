245d0ef60e471075a35a27a65b733350ba51ae98
Filip Szumski <filipszumski@gmail.com>
2021-07-08 16:45:01 +0200

    added heght and padding to html, body, and root

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,9 +3,20 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
+  height: 100%;
 }
 
 *, ::after, ::before {
   box-sizing: inherit;
 }
+
+body {
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+  padding: 30px 20px
+}
 `;
+
