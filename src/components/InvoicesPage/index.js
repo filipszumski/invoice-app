5b84ca1c7c1bd61dbf8393d883895e2e9dd9ee16
Filip Szumski <filipszumski@gmail.com>
2021-07-08 13:48:21 +0200

    changed main tag to styled component StyledMain and divided conent to two components Header and InvoiceList

--- src/components/InvoicesPage/index.js
+++ src/components/InvoicesPage/index.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { displayForm, setStatus } from "../../store/status/status";
-import { Button } from "../Button";
-import { toInvoice } from "../../shared/routes";
+import { useSelector } from "react-redux";
 import { Form } from "../Form";
 import { useFetchInvoices } from "./useFetchInvoices";
 import * as statusStage from "../../shared/consts/stages";
-import { Filter } from "./Filter";
+import { Header } from "./Header";
+import { InvoiceList } from "./InvoiceList";
+import { StyledMain } from "../StyledMain";
 
 export const InvoicesPage = () => {
     useFetchInvoices();
@@ -18,9 +16,7 @@ export const InvoicesPage = () => {
     ]);
 
     const status = useSelector((state) => state.status);
-    const dispatch = useDispatch();
     const checkedFilters = (filters.filter(item => item.checked)).map(object => object.name);
-    console.log(checkedFilters);
     const filtredInvoices = useSelector(state => {
         const invoices = state.invoices;
 
@@ -32,46 +28,23 @@ export const InvoicesPage = () => {
 
     return (
         <>
-            <main>
+            <StyledMain>
                 {status.stage === statusStage.loading
                     ? <p>Loading in progress...</p>
                     : status.stage === statusStage.error
                         ? <p>Error occurred</p>
                         : (<>
-                            <header>
-                                <div>
-                                    <h1>Invoices</h1>
-                                    <p>{filtredInvoices.length
-                                        ? `There are ${filtredInvoices.length} ${!checkedFilters.length || checkedFilters.length === filters.length
-                                            ? "total"
-                                            : checkedFilters.join(" & ")} invoices`
-                                        : "No invoices"}</p>
-                                </div>
-                                <div>
-                                    <Filter filters={filters} setFilters={setFilters} />
-                                    <Button onClick={() => dispatch(displayForm(true))} content="New Invoice" extraContent="+" />
-                                </div>
-                            </header>
-                            <section>
-                                {filtredInvoices.length === 0
-                                    ? <>
-                                        <h2>There is nothing here</h2>
-                                        <p>Create an invoice by clicking the <strong>New Invoice</strong> button and get started</p>
-                                    </>
-                                    : filtredInvoices.map((invoice) => (
-                                        <Link onClick={() => dispatch(setStatus(statusStage.loading))} key={invoice.id} to={toInvoice(invoice.id)}>
-                                            <p key={invoice.id}>
-                                                <span>{invoice.id}</span>{ }
-                                                <span>{invoice.paymentDue}</span>
-                                                <span>{invoice.clientName}</span>
-                                                <span>{invoice.total}</span>
-                                                <span>{invoice.status}</span>
-                                            </p>
-                                        </Link>
-                                    ))}
-                            </section>
+                            <Header
+                                filtredInvoices={filtredInvoices}
+                                checkedFilters={checkedFilters}
+                                filters={filters}
+                                setFilters={setFilters}
+                            />
+                            <InvoiceList
+                                filtredInvoices={filtredInvoices}
+                            />
                         </>)}
-            </main>
+            </StyledMain>
             <Form />
         </>
     )
