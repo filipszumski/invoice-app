fe141caeac4023336ce50bc96e1b572aeed357f5
Filip Szumski <filipszumski@gmail.com>
2021-07-13 20:03:39 +0200

    created theme file with themes data and, added ThemeProvider in App

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import { InvoicePage } from "./components/InvoicePage";
 import { Overlay } from "./components/common/Overlay/styled";
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { toInvoice, toInvoices } from "./shared/routes";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   const status = useSelector(state => state.status);
 
   return (
-    <HashRouter>
-      <Overlay active={status.overlayActive} />
-      <Switch>
-        <Route path={toInvoice()}>
-          <InvoicePage />
-        </Route>
-        <Route path={toInvoices()}>
-          <InvoicesPage />
-        </Route>
-        <Route path="/">
-          <Redirect to={toInvoices()} />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HashRouter>
+        <Overlay active={status.overlayActive} />
+        <Switch>
+          <Route path={toInvoice()}>
+            <InvoicePage />
+          </Route>
+          <Route path={toInvoices()}>
+            <InvoicesPage />
+          </Route>
+          <Route path="/">
+            <Redirect to={toInvoices()} />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,57 @@
+export const theme = {
+    colors: {
+        background: {
+            body: "#F2F2F2",
+            elements: "#FFFFFF;",
+            amountDueElement: "#373B53",
+            status: {
+                pending: "rgba(255, 143, 0, 0.1)",
+                paid: "rgba(51, 214, 159, 0.1)",
+                draft: "rgba(55, 59, 83, 0.1)",
+            }
+        },
+        hover: "#7C5DFA",
+        buttons: {
+            button1: "#7C5DFA",
+            button1Hover: "#9277FF",
+            button2: "#F9FAFE",
+            button2Hover: "#DFE3FA",
+            button3: "#373B53",
+            button3Hover: "#0C0E16",
+            button4: "#EC5757",
+            button4Hover: "#FF9797",
+            button5: "#F9FAFE",
+            button5Hover: "#DFE3FA",
+            color: "#FFFFFF",
+        },
+        border: "#9277FF",
+    },
+    breakpoints: {
+
+    },
+    font: {
+        color: {
+            normal: "#888EB0",
+            highlighted: "#0C0E16",
+            status: {
+                pending: "rgba(255, 143, 0, 1)",
+                paid: "rgba(51, 214, 159, 1)",
+                draft: "rgba(55, 59, 83, 1)",
+            },
+            amountDue: "#FFFFFF",
+        },
+        size: {
+            normal: 12,
+            keyInfo: 15,
+            title: 32,
+            subtitle: 22,
+        },
+        family: {
+            primary: "'Spartan', sans-serif",
+        },
+        weights: {
+            medium: 500,
+            bold: 700,
+        }
+    },
+};
