0f364f56620139727c6b7fdd80180f2a0b69a632
Filip Szumski <filipszumski@gmail.com>
2021-07-13 20:04:08 +0200

    changed styled in body and root

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
-  height: 100%;
 }
 
 *, ::after, ::before {
@@ -11,12 +10,19 @@ html {
 }
 
 body {
-  height: 100%;
+  line-height: 1.15;
+  background-color: ${({ theme }) => theme.colors.background.body};
+  font-family: ${({ theme }) => theme.font.family.primary};
+  font-size: ${({ theme }) => theme.font.size.normal}px;
+  color: ${({ theme }) => theme.font.color.normal};
 }
 
 #root {
-  height: 100%;
-  padding: 30px 20px
+  position: relative;
+  min-height: 100vh;
+  max-width: 750px;
+  margin: 0px auto;
+
 }
 `;
 
